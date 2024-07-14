d623257ffe3c1ca8d2828163e30253e3a10d8d8f
Dayana Uzunova <daqnauzunova@abv.bg>
2024-07-14 11:54:45 +0300

    Html template.
    Html template.

---
+++ Introduction to React and JSX/HTML-template/src/App.jsx
@@ -0,0 +1,31 @@
+import Navigation from "./components/Navigation"
+import Header from "./components/Header"
+import About from "./components/About"
+import WatchNow from "./components/WatchNow"
+import Features from "./components/Features"
+import OurTeam from "./components/OurTeam"
+import Testimonials from "./components/Testimonials"
+import FAQ from "./components/FAQ"
+import Contacts from "./components/Contacts"
+import Download from "./components/Download"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+<Navigation></Navigation>
+<Header></Header>
+<About></About>
+<WatchNow></WatchNow>
+<Features></Features>
+<OurTeam></OurTeam>
+<Testimonials></Testimonials>
+<FAQ></FAQ>
+<Contacts></Contacts>
+<Download></Download>
+<Footer></Footer>
+    </>
+  )
+}
+
+export default App
